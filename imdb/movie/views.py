74c7ab1dac3e8efa495d22a3dba94c2ae46c173f
Nijat <nmursali2019@ada.edu.az>
2020-03-24 14:44:19 +0400

    added views and models

--- imdb/movie/views.py
+++ imdb/movie/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+from .models import Movie
+
+class MovieList(ListView):
+    model = Movie
+
+class MovieDetail(DetailView):
+    model = Movie
