a98396d7d211f989b4db182d1337b1dca0bc1366
Nijat <nmursali2019@ada.edu.az>
2020-07-30 17:45:04 +0400

    new fixes

--- imdb/movie/models.py
+++ imdb/movie/models.py
@@ -15,6 +15,13 @@ STATUS_CHOICES = (
     ('MW', 'MOST WATCHED'),
     ('TR', 'TOP RATED'),
 )
+
+LINK_CHOICES = (
+    ('T', 'WATCH TRAILER'),
+    ('W', 'WATCH MOVIE'),
+    ('D', 'DOWNLOAD LINK'),
+)
+
 # Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=100)
@@ -24,7 +31,24 @@ class Movie(models.Model):
     language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2)
     status = models.CharField(choices=STATUS_CHOICES, max_length=2)
     yearofprod = models.DateField()
+    countryofprod = models.CharField(max_length=100, default="")
+    movieDirector = models.CharField(max_length=100, default="")
+    movieWriter = models.CharField(max_length=100, default="")
+    runtime = models.CharField(max_length=100, default= "")
     view_count = models.IntegerField(default=0)
+    ratings = models.FloatField(default=0)
+    trailerlink = models.URLField()
 
     def __str__(self):
         return  self.title
+
+
+class MovieLink(models.Model):
+    movie = models.ForeignKey(Movie, related_name='movie_watch_link', on_delete=models.CASCADE)
+    type = models.CharField(choices=LINK_CHOICES, max_length=1)
+    #movielink = models.URLField()
+    trailerlink = models.URLField()
+
+    def __str__(self):
+        return str(self.movie) + " | | " + str(self.type)
+    
--- imdb/movie/views.py
+++ imdb/movie/views.py
@@ -2,10 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView, DetailView
-from .models import Movie
+from .models import Movie, MovieLink
+
 
 class MovieList(ListView):
     model = Movie
+    paginate_by = 5
 
 class MovieDetail(DetailView):
     model = Movie
+
+    def get_object(self):
+        object = super(MovieDetail, self).get_object()
+        object.view_count += 1
+        object.save()
+        return object
+
+
+    def get_context_data(self, **kwargs):
+        context = super(MovieDetail, self).get_context_data(**kwargs)
+        context['links'] = MovieLink.objects.filter(movie=self.get_object())
+        return context
